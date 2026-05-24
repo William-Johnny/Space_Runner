833768c4665960abea9568470767d1b83743cd68
William-John <william@staays.com>
2026-05-24 22:20:44 +0200

    feat: added sound effects and music

--- Assets/Scripts/ObstacleParticals.cs
+++ Assets/Scripts/ObstacleParticals.cs
@@ -2,16 +2,27 @@ using UnityEngine;
 
 public class ObstacleParticals : MonoBehaviour
 {
-
     [SerializeField] ParticleSystem ps;
-
     [SerializeField] GameObject cube;
 
-    void OnCollisionEnter(Collision collision) {
-        if (collision.collider.tag == "Player") {
+    [SerializeField] AudioSource hitSound;
+
+    AudioSource musicSource;
+
+    void Start()
+    {
+        musicSource = GameObject.FindGameObjectWithTag("Music")
+                        .GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
             ps.Play();
+            musicSource.Stop();
+            hitSound.Play();
             Destroy(cube);
         }
     }
-    
 }
